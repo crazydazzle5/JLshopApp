df20719cc342337a3d3dd8fcb718352e3f8eb81a
tyxing1314 <ouyanghongbin>
2016-06-04 15:57:01 +0800

    111

---
+++ JLShop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JLShop
+//
+//  Created by imao on 16/5/22.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
