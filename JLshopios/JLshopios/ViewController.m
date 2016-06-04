5e5d6f899b7333d981ea2c7a078ce4939f29b34f
周玉玺 <578685136@qq.com>
2016-06-04 17:24:32 +0800

    简单的添加sb

--- JLshopios/JLshopios/ViewController.m
+++ JLshopios/JLshopios/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    MYLog(@"%@",@"哈哈哈");
+
 }
 
 - (void)didReceiveMemoryWarning {
